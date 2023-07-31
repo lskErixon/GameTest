a9b4b79f2147a3bf2ab81bbab75af39dde2b3d24
Erik <erik.lodkorikh@gmail.com>
2023-08-01 00:53:15 +0200

    add lvl

--- src/Hero.java
+++ src/Hero.java
@@ -1,6 +1,15 @@
 public class Hero {
     private int hP;
     private String name;
+    private int lvl;
+
+    public int getLvl() {
+        return lvl;
+    }
+
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
+    }
 
     public String getName() {
         return name;
